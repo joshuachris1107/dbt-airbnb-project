52ebc905a9a5e8a147bcd6bfe2ec55550dc178c7
Joshua <joshuaalexander1107@gmail.com>
2026-06-12 00:15:26 +0530

    added new macros and packages

---
+++ macros/no_empty_strings.sql
@@ -0,0 +1,9 @@
+{% macro no_empty_strings(model) %}
+
+{% for col in adapter.get_columns_in_relation(model) %}
+    {% if col.is_string() %}
+    {{col.name}} is not null and  {{col.name}} <> '' AND
+    {% endif %}
+{% endfor %}
+true
+{% endmacro %}
