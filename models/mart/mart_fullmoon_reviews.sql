a8130390ff1bf286b7f84b3d58d79a2dd8d48231
Joshua <joshuaalexander1107@gmail.com>
2026-06-25 23:26:10 +0530

    feat: add generate_schema_name macro for dev/prod schema routing

--- models/mart/mart_fullmoon_reviews.sql
+++ models/mart/mart_fullmoon_reviews.sql
@@ -1,6 +1,7 @@
 {{ config(
   materialized = 'table',
-  tags = ['fact']
+  tags = ['fact'],
+  schema = 'mart'
 ) }}
 
 WITH fct_reviews AS (
--- tests/generic/minimum_row_count.sql
+++ tests/generic/minimum_row_count.sql
@@ -1,4 +1,5 @@
 {% test minimum_row_count(model, min_rows) %}
+{ config( severity = 'warn') }
 Select count(*) as cnt 
 from {{model}} having count(*) < {{min_rows}}
 {% endtest %}
